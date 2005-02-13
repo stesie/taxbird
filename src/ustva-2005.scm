aebc69fda1bbf184b53a4f76a151b981fb360305
stesie <stesie>
2005-02-13 19:15:12 +0000

    more sheets added.

--- src/ustva-2005.scm
+++ src/ustva-2005.scm
@@ -170,25 +170,209 @@
 		#f ; no desc for 2nd field ...
 		"Umsätze, die anderen Steuersätzen unterliegen (Steuer)"
 		(lambda(val buf)
-		  (if (validate:signed-monetary val buf)
-		      (let ((kz35val (storage:retrieve buf "Kz35")))
-			(if (= (string-length val) 0) (set! val "0"))
-			(if (or (not (string? kz35val))
-				(= (string-length kz35val) 0))
-			    (set! kz35val "0"))
-			(if (< (string->number val) (string->number kz35val))
-			    #t
-			    #f))
-		      #f)))))))
+		  (validate:signed-monetary-max val buf
+						(storage:retrieve buf
+								  "Kz35")))))
 
 
 
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    "Umsätze nach § 24 UStG"
+    (list tb:field:text-input
+	  (list "Kz77"
+		"Lief. übriges Gemeinschaftsgeb., Abn. mit USt-IdNr."
+		(string-append "Umsätze land- und forstwirtschaftl. Betriebe "
+			       "nach § 24 UStG; Lieferungen in das übrige "
+			       "Gemeinschaftsgebiet an Abnehmer *mit* "
+			       "USt-IdNr.")
+		validate:signed-int)
+
+
+	  tb:field:text-input-input
+	  (list "Kz76"
+		"Umsätze, Steuer nach § 24 UStG"
+		(string-append "Umsätze land- und forstwirtschaftl. Betriebe "
+			       "nach § 24 UStG; Umsätze für die eine Steuer "
+			       "nach § 24 UStG zu entrichten ist (Sägewerks"
+			       "erzeugnisse, Getränke und alkohol. "
+			       "Flüssigkeiten, z. B. Wein) - Umsatz")
+		validate:signed-int
+
+		;; second field ...
+		"Kz80"
+		#f ; no label ther ...
+		(string-append "Umsätze land- und forstwirtschaftl. Betriebe "
+			       "nach § 24 UStG; Umsätze für die eine Steuer "
+			       "nach § 24 UStG zu entrichten ist (Sägewerks"
+			       "erzeugnisse, Getränke und alkohol. "
+			       "Flüssigkeiten, z. B. Wein) - Steuer")
+		(lambda (val b)
+		  (validate:signed-monetary-max val b
+						(storage:retrieve b "Kz76"))))))
 
 
 
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   "Innergemeinschaftliche Erwerbe"
+   (list tb:field:text-input
+	 (list "Kz91" 
+	       "steuerfrei, § 4b UStG"
+	       "Steuerfreie innergemeinschaftliche Erwerbe nach § 4b UStG" 
+	       validate:signed-int)
+	 
+	 
+	 tb:field:text-input-calc
+	 (list "Kz97"
+	       "zum Steuersatz von 16%"
+	       (string-append "Steuerpflichtige innergemeinschaftliche Erwerbe "
+			      "zum Steuersatz von 16 v.H.")
+	       validate:signed-int
+	       
+	       ;; second field ...
+	       "Kz97-calc")
+
+	  
+	 tb:field:text-input-calc
+	 (list "Kz93"
+	       "zum Steuersatz von 7%"
+	       (string-append "Steuerpflichtige innergemeinschaftliche Erwerbe "
+			      "zum Steuersatz von 7 v.H.")
+	       validate:signed-int
+	       
+	       ;; second field ...
+	       "Kz93-calc")
+	 
+	 
+	 tb:field:text-input-input
+	 (list "Kz95"
+	       "andere Steuersätze"
+	       (string-append "Steuerpflichtige innergemeinschaftliche Erwerbe,"
+			      " die anderen Steuersätzen unterliegen (Umsatz)")
+	       validate:signed-int
+
+	       ;; second field
+	       "Kz98"
+	       #f ; no desc for 2nd field ...
+	       (string-append "Steuerpflichtige innergemeinschaftliche Erwerbe,"
+			      " die anderen Steuersätzen unterliegen (Steuer)")
+	       (lambda(val buf)
+		 (validate:signed-monetary-max val buf
+					       (storage:retrieve buf "Kz95"))))
+	 
+
+	 tb:field:text-input-input
+	 (list "Kz94"
+	       "neue Fahrz., ohne USt-IdNr."
+	       (string-append "Steuerpflichtige innergemeinschaftliche Erwerbe,"
+			      " neuer Fahrzeuge von Lieferern *ohne* USt-IdNr. "
+			      "zum allgemeinen Steuersatz (Umsatz)")
+	       validate:signed-int
+	       
+	       ;; second field
+	       "Kz96"
+	       #f ; no desc for 2nd field ...
+	       (string-append "Steuerpflichtige innergemeinschaftliche Erwerbe,"
+			      " neuer Fahrzeuge von Lieferern *ohne* USt-IdNr. "
+			      "zum allgemeinen Steuersatz (Steuer)")
+	       (lambda(val buf)
+		 (validate:signed-monetary-max val buf
+					       (storage:retrieve buf "Kz94")))))
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   "Ergänzende Angaben zu Umsätzen"
+   (list tb:field:text-input
+	 (list "Kz42"
+	       "Lief. d. 1. Abn. bei innerg. Dreiecksgesch."
+	       (string-append "Umsätze aus Lieferungen des ersten Abnehmers "
+			      "bei *innergemeinschaftlichen Dreiecksgeschäften*"
+			      " (§ 25b Abs. 2 UStG)")
+	       validate:signed-int)
+
+
+	 tb:field:text-input
+	 (list "Kz60"
+	       "Steuerpfl. Umsätze § 13b UStG"
+	       (string-append "Steuerpflichtige Umsätze im Sinne des § 13b "
+			      "Abs. 1 Satz 1 Nr. 1 bis 5 UStG, für die der "
+			      "*Leistungsempfänger* die *Steuer schuldet*")
+	       validate:signed-int)
+
+	 
+	 tb:field:text-input
+	 (list "Kz45"
+	       "im Inland nicht steuerbare Umsätze"
+	       "im Inland nicht steuerbare Umsätze"
+	       validate:signed-int))
 
 
 
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+   "Umsätze § 13b Abs. 2 UStG"
+   (list tb:field:text-input-input
+	 (list "Kz52"
+	       "Leistg. v. Untern. im Ausland"
+	       (string-append "Leistungen eines im Ausland ansässigen "
+			      "Unternehmers (§ 13b Abs. 1 Satz 1 Nr. 1 und 5 "
+			      "UStG), für die als Leistungsempfänger "
+			      "Steuer geschuldet wird - Umsatz")
+	       validate:signed-int
+
+	       ;; 2nd field
+	       "Kz53"
+	       #f ;; no label
+	       (string-append "Leistungen eines im Ausland ansässigen "
+			      "Unternehmers (§ 13b Abs. 1 Satz 1 Nr. 1 und 5 "
+			      "UStG), für die als Leistungsempfänger "
+			      "Steuer geschuldet wird - Steuer")
+	       (lambda (val b)
+		 (validate:signed-monetary-max val b
+					       (storage:retrieve b "Kz52"))))
+
+
+	 tb:field:text-input-input
+	 (list "Kz73"
+	       "Lief. sichergsübereig. Gegenst., Ums. GrEStG"
+	       (string-append "Lieferungen sicherungsübereigneter Gegenstände "
+			      "und Umsätze, die unter das GrEStG fallen "
+			      "(§ 13b Abs. 1 Satz 1 Nr. 2 und 3 UStG), für die "
+			      "als Leistungsempfänger Steuer geschuldet "
+			      "wird - (Umsatz)")
+	       validate:signed-int
+
+	       ;; 2nd field
+	       "Kz74"
+	       #f ;; no label
+	       (string-append "Lieferungen sicherungsübereigneter Gegenstände "
+			      "und Umsätze, die unter das GrEStG fallen "
+			      "(§ 13b Abs. 1 Satz 1 Nr. 2 und 3 UStG), für die "
+			      "als Leistungsempfänger Steuer geschuldet "
+			      "wird - (Steuer)")
+	       (lambda (val b)
+		 (validate:signed-monetary-max val b
+					       (storage:retrieve b "Kz73"))))
+
+
+	 tb:field:text-input-input
+	 (list "Kz84"
+	       "Bauleistungen eines Untern. im Inland"
+	       (string-append "Bauleistungen eines im Inland ansässigen "
+			      "Unternehmers (§ 13b Abs. 1 Satz 1 Nr. 4 UStG), "
+			      "für die als Leistungsempfänger Steuer geschuldet"
+			      " wird - (Umsatz)")
+	       validate:signed-int
+
+	       ;; 2nd field
+	       "Kz85"
+	       #f ;; no label
+	       (string-append "Bauleistungen eines im Inland ansässigen "
+			      "Unternehmers (§ 13b Abs. 1 Satz 1 Nr. 4 UStG), "
+			      "für die als Leistungsempfänger Steuer geschuldet"
+			      " wird - (Steuer)")
+	       (lambda (val b)
+		 (validate:signed-monetary-max val b
+					       (storage:retrieve b "Kz84")))))))
+    
 
 
 (define ustva-2005:recalculate
@@ -198,7 +382,13 @@
 					  (/ (* (string->number v) 16) 100)))
 		  "Kz86" (lambda(v buffer)
 			   (storage:store buffer "Kz86-calc"
-					  (/ (* (string->number v) 7) 100))))))
+					  (/ (* (string->number v) 7) 100)))
+		  "Kz93" (lambda(v buffer)
+			   (storage:store buffer "Kz93-calc"
+					  (/ (* (string->number v) 7) 100)))
+		  "Kz97" (lambda(v buffer)
+			   (storage:store buffer "Kz97-calc"
+					  (/ (* (string->number v) 16) 100))))))
 
       (while (> (length list) 0)
 	     (if (string=? (car list) element)
