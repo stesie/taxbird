55e7590d8de70cbeb73f2a4d3329b8822769424b
stesie <stesie>
2005-04-23 16:03:49 +0000

    * further validation code added (mainly plz, ort, land fields)  * slight changes to documentation text  * generate Kz09 in export output only, if berufsbez. field is filled

--- src/guile/basics-datenlieferant.scm
+++ src/guile/basics-datenlieferant.scm
@@ -17,6 +17,32 @@
 
 (define validate:datenlieferant
   (lambda (value buffer field)
+    (and (if (let ((berufsbez (if (string=? field "berufsbez")
+				  value
+				  (storage:retrieve buffer "berufsbez"))))
+
+	       (and (string? berufsbez)
+		    (> (string-length berufsbez) 0)))
+
+	     ;; berufsbez. set, therefore we'll try to generate a full kz09
+	     (validate:kz09-maxlen value buffer field)
+
+	     ;; we're not going to generate kz09, therefore don't care for
+	     ;; maximum length ...
+	     #t)
+
+	 (if (string=? field "berufsbez")
+	     #t ;; empty field would be okay (won't generate kz09 in case)
+
+	     (and 
+	      (string? value) ;;; in other case, make sure we've got a 
+	                      ;;; string associated, that's at least one
+	                      ;;; character long.
+	      (> (string-length value) 0))))))
+
+
+(define validate:kz09-maxlen
+  (lambda (value buffer field)
     ;; all the fields in 'datenlieferant' sheet together must not be longer
     ;; the 90 chars ..
     (let ((totallen (if (string? value) (string-length value) 0))
@@ -52,24 +78,24 @@
 	      (lambda(val buf) 
 		(validate:datenlieferant val buf "name-lieferant")))
     
-	(list "Berufsbezeichnung"
+	(list "Berufsbezeichnung\ndes Beraters"
 	      tb:field:text-input
 	      "berufsbez"
-	      "Berufsbezeichnung des Beraters"
+	      "Berufsbezeichnung des Steuerberaters (falls zutreffend)"
 	      (lambda(val buf)
 		(validate:datenlieferant val buf "berufsbez")))
     
 	(list "Tel.-Nr. (Vorwahl)"
 	      tb:field:text-input
 	      "vorwahl" 
-	      "Tel.-Nr. Datenlieferant/Berater (Vorwahl)"
+	      "Tel.-Nr. des Datenlieferanten bzw. Beraters (Vorwahl)"
 	      (lambda(val buf)
 		(validate:datenlieferant val buf "vorwahl")))
 
 	(list "Tel.-Nr. (Anschluss)"
 	      tb:field:text-input
 	      "anschluss" 
-	      "Tel.-Nr. Datenlieferant/Berater (Anschluss)"
+	      "Tel.-Nr. des Datenlieferanten bzw. Beraters (Anschluss)"
 	      (lambda(val buf)
 		(validate:datenlieferant val buf "anschluss")))
 
@@ -77,26 +103,29 @@
 	      tb:field:text-input
 	      "plz-lieferant" 
 	      "PLZ des Datenlieferanten" 
-	      #t) ;; FIXME, validation!!
+	      (lambda(val buf)
+		(validate:datenlieferant val buf "plz-lieferant")))
 
 	(list "Ort"
 	      tb:field:text-input
 	      "ort-lieferant"
 	      "Sitzort des Datenlieferanten" 
-	      #t) ;; FIXME, validation!!
+	      (lambda(val buf)
+		(validate:datenlieferant val buf "ort-lieferant")))
 
 	(list "Land" 
 	      tb:field:text-input
 	      "land-lieferant" 
 	      "Land des Datenlieferanten"
-	      #t) ;; FIXME, validation
+	      (lambda(val buf)
+		(validate:datenlieferant val buf "land-lieferant")))
 
 	(list "Name Mandant"
 	      tb:field:text-input
 	      "mandant" 
 	      "Name des Mandanten (optional, sofern zutreffend aber erwünscht)"
 	      (lambda(val buf)
-		(validate:datenlieferant val buf "mandant")))))
+		(validate:kz09-maxlen val buf "mandant")))))
 
 
 (define export:generate-datenlieferant
@@ -113,9 +142,20 @@
 
 (define export:generate-kz09
   (lambda (store)
-    (string-append (storage:retrieve store "vend-id") "*"
-		   (storage:retrieve store "name-lieferant") "*"
-		   (storage:retrieve store "berufsbez") "*"
-		   (storage:retrieve store "vorwahl") "*"
-		   (storage:retrieve store "anschluss") "*"
-		   (storage:retrieve store "mandant"))))
+    (let ((berufsbez (storage:retrieve store "berufsbez")))
+      (if (and berufsbez
+	       (> (string-length berufsbez) 0))
+
+	  ;; berufsbez. is set, i.e. there's some advisor, that files this
+	  ;; tax declaration -> write out kz09 tag ...
+	  (string-append (storage:retrieve store "vend-id") "*"
+			 (storage:retrieve store "name-lieferant") "*"
+			 berufsbez "*"
+			 (storage:retrieve store "vorwahl") "*"
+			 (storage:retrieve store "anschluss") "*"
+			 (storage:retrieve store "mandant"))
+
+	  ;; berufsbez. is not set - as it shall be set only for tax 
+	  ;; advisors etc., we can assume the person who sends the data
+	  ;; is the same as the person who has to pay, i.e. don't fill kz09
+	  (storage:retrieve store "vend-id")))))
