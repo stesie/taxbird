bcaf5391e6ece6071744de237739551ba383221a
stesie <stesie>
2005-05-08 17:17:35 +0000

    implementing on_file_close_activate

--- src/callbacks.c
+++ src/callbacks.c
@@ -127,7 +127,8 @@ on_file_quit_activate(GtkMenuItem *menuitem, gpointer user_data)
   (void) menuitem;
   (void) user_data;
 
-  gtk_main_quit();
+  g_slist_foreach(taxbird_windows, (GFunc)on_file_close_activate, NULL);
+  /* gtk_main_quit(); */
 }
 
 
@@ -180,8 +181,18 @@ on_choose_template_OK_clicked(GtkButton *button, gpointer user_data)
   }
 
   appwindow = gtk_object_get_user_data(GTK_OBJECT(dialog));
-  taxbird_ws_sel_form(appwindow, taxbird_form_get_by_name(sel_form_name));
 
+  /* check whether the passed taxbird appwin reference already has a 
+   * template associated, if yes, create a new window */
+  int cf = (int) g_object_get_data(G_OBJECT(appwindow), "current_form");
+  if(cf != -1)
+    appwindow = taxbird_ws_new();
+
+  if(appwindow)
+    taxbird_ws_sel_form(appwindow, taxbird_form_get_by_name(sel_form_name));
+  else
+    taxbird_dialog_error(NULL, _("Unable to create new document. Sorry."));
+ 
   g_free(sel_form_name);
   gtk_widget_destroy(dialog);
 }
@@ -257,7 +268,52 @@ on_file_send_activate(GtkMenuItem *menuitem, gpointer user_data)
   }
 
   taxbird_export(aw);
+}
 
+
+
+gboolean
+on_file_close_activate(GtkWidget *widget, GdkEvent *event, gpointer user)
+{
+  GtkWidget *aw = lookup_widget(GTK_WIDGET(widget), "taxbird");
+  int changed = (int)g_object_get_data(G_OBJECT(aw), "changed");
+
+  if(changed) {
+    /* ask the user whether to save the current file */
+    const char *fn = g_object_get_data(G_OBJECT(aw), "filename");
+    if(! fn) fn = _("(yet unnamed)");
+
+    char *msg = g_strdup_printf(_("Your recent changes to the document %s "
+				  "haven't been stored to disk so far. Do "
+				  "you want them to be stored now?"), fn);
+
+    int resp = taxbird_dialog_yes_no_cancel(aw, msg);
+    g_free(msg);
+
+    switch(resp) {
+    case GTK_RESPONSE_YES:
+      on_file_save_activate(widget, NULL);
+
+      /* re-read changed flag, abort if changes haven't been saved */
+      changed = (int)g_object_get_data(G_OBJECT(aw), "changed");
+      if(changed) return; /* abort */
+      break;
+
+    case GTK_RESPONSE_NO:
+      break;
+
+    default:
+      return TRUE; /* don't destroy this window */
+    }
+  }
+
+  gtk_widget_destroy(aw);
+  taxbird_windows = g_slist_remove(taxbird_windows, aw);
+
+  /* stop the main loop, in case this was the last window */
+  if(! taxbird_windows) gtk_main_quit();
+
+  return FALSE;
 }
 
 
