319f61b25470d0355f733626fb613de1e80cdf3d
stesie <stesie>
2005-10-08 18:15:36 +0000

    changed update target to `mohawk'

--- src/taxbird-mark-stable.sh
+++ src/taxbird-mark-stable.sh
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 CVS=/usr/bin/cvs
-STABLE=openssl_stable
+STABLE=mohawk
 
 if test "$1" = "--help" -o "$1" = "-h"; then
     cat <<EOF
